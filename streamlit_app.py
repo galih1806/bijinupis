831c3c7be28b511d72771a6859afe5cc0eac4d81
galih1806 <127742624+galih1806@users.noreply.github.com>
2025-04-24 13:20:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("BIJINUPIS MC DEWEE")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Balajar menjadi rapper gaya Brazil"
 )
