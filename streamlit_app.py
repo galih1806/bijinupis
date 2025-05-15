983e18289b49ead0a6c0294c03fd9eb5c873f015
galih1806 <127742624+galih1806@users.noreply.github.com>
2025-05-15 14:39:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Selamat Datang di Website MC DEWEE")
-st.image("IMG-20250509-WA0036.jpg", width=200)
+st.image("IMG-20250509-WA0036.jpg",width=200)
 st.write("/n")
 st.subheader("Redemptus Galih aka MC DEWEE")
 st.write("Mari belajar menjadi rapper Brazil yang terampil")
